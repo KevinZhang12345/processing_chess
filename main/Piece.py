c4140b5e0df2cade47aeb835c0bdce7fe8af9ab7
KevinZhang12345 <100251615+KevinZhang12345@users.noreply.github.com>
2023-01-13 22:14:31 -0500

    Update Piece.py

--- main/Piece.py
+++ main/Piece.py
@@ -18,16 +18,6 @@ class Piece(object):
                 if element != None and element is self:
                     self.x = row
                     self.y = col
-                    
-                    
-    def move(self, board, destination):
-        if board.squares[self.x][self.y] is self:
-            board.squares[destination[0]][destination[1]] = self
-            board.squares[self.x][self.y] = None
-            self.x, self.y = destination[0], destination[1]
-        else: 
-            return False
-        
     
 class Pawn(Piece):
     def __init__(self, colour):
@@ -44,17 +34,55 @@ class Pawn(Piece):
                 moves.append((-2,0))
                 
         if self.colour == "black":
-            print("test")
             moves.append((1,0))
             
             if not self.hasMoved:
                 moves.append((2, 0))
-
+                    
         for move in moves:
             newPosition = (self.x + move[0], self.y + move[1])
             if newPosition[1] < 8 and newPosition[1] >= 0:
                 moveList.append(newPosition)
         return moveList
+    
+    def getMoves(self, board):
+        moveList = []
+        
+        for destination in self.possibleMoves(board):
+            if board.squares[destination[0]][destination[1]] != None:
+                break
+            else:
+                moveList.append(destination)
+
+        if self.colour == 'white':
+            if self.x - 1 >=0 and self.y + 1 < 8:
+                destination = board.squares[self.x-1][self.y+1]
+                if destination != None:
+                    if destination.colour != self.colour:
+                        moveList.append((self.x-1, self.y+1))
+                        
+            if self.x - 1 >= 0 and self.y - 1>=0:
+                destination = board.squares[self.x-1][self.y-1]
+                if destination != None:
+                    if destination.colour != self.colour:
+                        moveList.append((self.x-1, self.y-1))
+                        
+        if self.colour == 'black':
+            if self.x + 1 < 8 and self.y + 1 < 8:
+                destination = board.squares[self.x+1][self.y+1]
+                if destination != None:
+                    if destination.colour != self.colour:
+                        moveList.append((self.x+1, self.y+1))
+                        
+            if self.x + 1 < 8 and self.y - 1>=0:
+                destination = board.squares[self.x+1][self.y-1]
+                if destination != None:
+                    if destination.colour != self.colour:
+                        moveList.append((self.x+1, self.y-1))
+                        
+        return moveList
+                
+    
 
 class Knight(Piece):
      def __init__(self, colour):
